e5205245344f728568a4351643385b38fc5b280d
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-05-12 00:15:26 -0300

    fixtures products e inclusão no admin do django

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'brand', 'created', 'modified')
+    # list_filter = ()
+    # search_fields = ()
+    ordering = ('-modified', '-created')
+
+
+@admin.register(ProductBrand)
+class ProductBrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'created', 'modified')
+    # list_filter = ()
+    # search_fields = ()
+    ordering = ('-modified', '-created')
+
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating', 'product', 'created', 'modified')
+    # list_filter = ()
+    # search_fields = ()
+    ordering = ('-modified', '-created')
+
+
+@admin.register(UserFavorites)
+class UserFavoritesAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_favorites', 'created', 'modified')
+    # list_filter = ()
+    # search_fields = ()
+    ordering = ('-modified', '-created')
+
+    def get_favorites(self, obj):
+        """
+        Retorna a quantidade de favoritos para este user
+        """
+        return len(obj.products.all())
