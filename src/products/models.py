6badd3174336c364c944087bbb983c231c4fb31e
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-05-12 00:15:11 -0300

    Ajuste de model products

--- src/products/models.py
+++ src/products/models.py
@@ -20,11 +20,8 @@ class ProductBrand(DefaultModel):
     name = models.CharField(max_length=256, default='Nova Marca')
     category = models.PositiveIntegerField(choices=CATEGORIES, default=0)
 
-
-class ProductReview(DefaultModel):
-    user = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_NULL)
-    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
-    description = models.TextField(default='', blank=True)
+    def __str__(self):
+        return self.name
 
 
 class Product(DefaultModel):
@@ -36,12 +33,27 @@ class Product(DefaultModel):
         default='https://www.pngkey.com/png/full/75-754812_question-mark-image-point-d-interrogation-png.png'
     )
     brand = models.ForeignKey(ProductBrand, default=None, null=True, on_delete=models.SET_NULL)
-    reviews = models.ManyToManyField(ProductReview)
+
+    def __str__(self):
+        return self.title
+
+
+class ProductReview(DefaultModel):
+    user = models.ForeignKey(User, default=None, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, default=None, null=True, on_delete=models.SET_NULL)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    description = models.TextField(default='', blank=True)
+
+    def __str__(self):
+        return f'({self.pk}) {self.user} - {self.rating}'
 
 
 class UserFavorites(DefaultModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, symmetrical=False, blank=False)
+
+    def __str__(self):
+        return self.user.email
 
 
 @receiver(post_save, sender=User)
