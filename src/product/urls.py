750e12474b16fe461536671b31880cb05b7990a6
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-05-16 02:18:09 -0300

    retornando dados de um produto específico por rota

--- src/product/urls.py
+++ src/product/urls.py
@@ -6,4 +6,5 @@ app_name = 'product'
 urlpatterns = [
     # API REST
     path('', Products.as_view(), name='products_index'),
+    path('<int:product_pk>/', Products.as_view(), name='product_by_pk'),
 ]
--- src/product/views.py
+++ src/product/views.py
@@ -14,26 +14,40 @@ class Products(APIView):
 
     def get(self, request, *args, **kwargs):
         """
+        Caso tenha query params, busca em todos os produtos de acordo com os params válidos
+        Caso esteja na rota "product/<:id>" retorna os dados do produto caso a pk exista
         Ex. GET http://localhost:8000/product/?title=a
+        Ex. GET http://localhost:8000/product/1
         """
-        # TODO refatorar os métodos GET para receberem os query params e model a ser consultado (evitar código duplicado)
-        query_params = ['title', 'price', 'brand', 'page']
-        request_get_keys = list(request.GET.keys())
-        if any(query_param in request_get_keys for query_param in query_params):
-            if 'page' not in request_get_keys:
-                request.GET['page'] = '1'
-            filter_kwargs = dict()
-            for query_param in request_get_keys:
-                if query_param != 'page':
-                    filter_kwargs[query_param + '__icontains'] = request.GET[query_param]
-            products = Product.objects.filter(**filter_kwargs)
-            paginator = PageNumberPagination()
-            paginator.page_size = 10
-            result_page = paginator.paginate_queryset(products, request)
-            serializer = ProductSerializer(result_page, many=True)
-            return JsonResponse(serializer.data, safe=False)
+        if self.kwargs.get('product_pk'):
+            try:
+                product = Product.objects.filter(pk=self.kwargs.get('product_pk'))
+                if product.exists():
+                    serializer = ProductSerializer(product[0], many=False)
+                    return JsonResponse(serializer.data, safe=False)
+                else:
+                    return GeneralApiResponse.bad_request()
+            except:
+                return GeneralApiResponse.bad_request()
         else:
-            return GeneralApiResponse.bad_request('favor inserir algum parâmetro de busca válido')
+            # TODO refatorar os métodos GET para receberem os query params e model a ser consultado (evitar código duplicado)
+            query_params = ['title', 'price', 'brand', 'page']
+            request_get_keys = list(request.GET.keys())
+            if any(query_param in request_get_keys for query_param in query_params):
+                paginator = PageNumberPagination()
+                paginator.page_size = 10
+                if 'page' not in request_get_keys:
+                    paginator.page = 1
+                filter_kwargs = dict()
+                for query_param in request_get_keys:
+                    if query_param != 'page':
+                        filter_kwargs[query_param + '__icontains'] = request.GET[query_param]
+                products = Product.objects.filter(**filter_kwargs)
+                result_page = paginator.paginate_queryset(products, request)
+                serializer = ProductSerializer(result_page, many=True)
+                return JsonResponse(serializer.data, safe=False)
+            else:
+                return GeneralApiResponse.bad_request('favor inserir algum parâmetro de busca válido')
 
     def post(self, request, *args, **kwargs):
         return GeneralApiResponse.bad_request()
